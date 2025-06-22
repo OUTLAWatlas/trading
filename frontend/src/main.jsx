6998bb376b20cf5306442ddb2e42cba4dab22ac2
Krishna Patil <krishna.patil241@vit.edu>
2025-06-23 03:44:10 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
+import Dashboard from './pages/dashboard';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
